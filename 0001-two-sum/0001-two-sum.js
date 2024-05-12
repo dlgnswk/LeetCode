a942edf1c416480a795b6dbe31e461b5646e4353
Lee Jaehoon <99021245+dlgnswk@users.noreply.github.com>
2024-05-12 10:18:43 +0900

    Time: 147 ms (11.97%), Space: 49.4 MB (76.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,9 +4,9 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 1; j <= nums.length - 1; j++) {
-      if (nums[i] + nums[j] === target && i !== j) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j <= nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
         return [i, j];
       }
     }
