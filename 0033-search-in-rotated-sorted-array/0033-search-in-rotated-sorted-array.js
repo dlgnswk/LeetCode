bd65c339737aac8e45b221159cc22c0e22d1f1f2
Jaehoon Lee <99021245+dlgnswk@users.noreply.github.com>
2024-09-15 14:37:00 +0900

    Time: 61 ms (14.64%), Space: 49.1 MB (31.33%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -4,5 +4,30 @@
  * @return {number}
  */
 var search = function(nums, target) {
-  return nums.indexOf(target);
+  if (nums.length === 0) return -1;
+  
+  let left = 0;
+  let right = nums.length - 1;
+  
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    
+    if (target === nums[mid]) return mid;
+    
+    if (nums[left] <= nums[mid]) {
+      if (nums[left] <= target && target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      if (nums[mid] < target && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+  
+  return -1;
 };
