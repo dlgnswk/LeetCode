3d209c1775adf7e709f2e414236ccff10fcb3c48
Jaehoon Lee <99021245+dlgnswk@users.noreply.github.com>
2024-09-20 13:36:05 +0900

    Time: 395 ms (18.37%), Space: 96.4 MB (16.62%) - LeetHub

---
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.js
@@ -0,0 +1,52 @@
+
+var TimeMap = function() {
+  this.store = new Map();
+};
+
+/** 
+ * @param {string} key 
+ * @param {string} value 
+ * @param {number} timestamp
+ * @return {void}
+ */
+TimeMap.prototype.set = function(key, value, timestamp) {
+  if (!this.store.has(key)) {
+    this.store.set(key, []);
+  }
+  
+  this.store.get(key).push([timestamp, value]);
+};
+
+/** 
+ * @param {string} key 
+ * @param {number} timestamp
+ * @return {string}
+ */
+TimeMap.prototype.get = function(key, timestamp) {
+  if (!this.store.has(key)) return "";
+  
+  const values = this.store.get(key);
+  let left = 0;
+  let right = values.length - 1;
+  let result = "";
+  
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    
+    if (values[mid][0] <= timestamp) {
+      result = values[mid][1];
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  
+  return result;
+};
+
+/** 
+ * Your TimeMap object will be instantiated and called as such:
+ * var obj = new TimeMap()
+ * obj.set(key,value,timestamp)
+ * var param_2 = obj.get(key,timestamp)
+ */
